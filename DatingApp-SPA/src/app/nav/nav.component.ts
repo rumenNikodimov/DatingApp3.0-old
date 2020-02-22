eefaace0947d73c6d94fffb69448ba7c119ed95c
Rumen Nikodimov <ru33@abv.bg>
2020-02-22 22:44:15 +0200

    second commit

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authServise: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authServise.login(this.model).subscribe(next => {
+        console.log('Logged in successfull');
+    }, error => {
+        console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    console.log('logged out');
+  }
+}
